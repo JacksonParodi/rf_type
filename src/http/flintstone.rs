af8dbbd1d7cb3acb3796db2c4554455229af234a
Jackson Parodi <jacksonaparodi@gmail.com>
2025-05-02 04:01:50 -0400

    initial commit

---
+++ src/http/flintstone.rs
@@ -0,0 +1,25 @@
+enum FlintstoneData {
+    Get,
+    Post(u64),
+}
+
+pub struct FlintstoneRequest {
+    pub data: FlintstoneData,
+}
+
+impl FlintstoneRequest {
+    pub fn get() -> FlintstoneRequest {
+        FlintstoneRequest {
+            data: FlintstoneData::Get,
+        }
+    }
+    pub fn post(data: u64) -> FlintstoneRequest {
+        FlintstoneRequest {
+            data: FlintstoneData::Post(data),
+        }
+    }
+}
+
+pub struct FlintstoneResponse {
+    pub count: u64,
+}
