a85d87b41a5c8d6003c5d51c3382c7afeced591c
Jackson Parodi <jacksonaparodi@gmail.com>
2025-05-09 18:08:30 -0400

    add status code to response

--- src/http/response.rs
+++ src/http/response.rs
@@ -17,6 +17,7 @@ pub enum ResponsePayload {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct HttpResponse {
+    pub code: u16,
     pub status: String,
     pub message: String,
     pub data: Option<ResponsePayload>,
