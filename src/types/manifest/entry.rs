b98082d395e51848267e4546122334f98c5d9218
Jackson Parodi <jacksonaparodi@gmail.com>
2025-05-16 15:05:56 -0400

    find item method

--- src/types/manifest/entry.rs
+++ src/types/manifest/entry.rs
@@ -1,6 +1,7 @@
 use crate::error::RfError;
 use serde::{Deserialize, Serialize};
 use std::{fs, path::PathBuf};
+use tracing::warn;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ManifestEntry {
@@ -16,6 +17,7 @@ impl ManifestEntry {
             children: None,
         }
     }
+
     pub fn generate_recursive_manifest(path: &PathBuf) -> Result<Self, RfError> {
         fn build_manifest(path: &PathBuf) -> Result<ManifestEntry, RfError> {
             let metadata = fs::metadata(path).expect("failed to read metadata");
@@ -40,6 +42,47 @@ impl ManifestEntry {
         match build_manifest(&path) {
             Ok(manifest) => Ok(manifest),
             Err(e) => Err(RfError::IoError(e.to_string())),
+        }
+    }
+
+    pub fn find_item(&self, file_name: &str) -> Option<PathBuf> {
+        let mut result = None;
+        if self.path.file_name()?.to_str()? == file_name {
+            result = Some(self.path.clone());
+        }
+        if let Some(children) = &self.children {
+            for child in children {
+                if let Some(path) = child.find_item(file_name) {
+                    result = Some(path);
+                }
+            }
+        }
+
+        match result {
+            Some(path) => {
+                let mut seen_asset = false;
+                let mut after_asset = Vec::new();
+
+                for comp in path.components() {
+                    if seen_asset {
+                        after_asset.push(comp.as_os_str());
+                    } else if comp
+                        .as_os_str()
+                        .to_string_lossy()
+                        .eq_ignore_ascii_case("asset")
+                    {
+                        seen_asset = true;
+                    }
+                }
+                if !seen_asset {
+                    warn!("Asset not found in path: {:?}", path);
+                    return None;
+                }
+
+                let relative_path = after_asset.iter().collect();
+                Some(relative_path)
+            }
+            None => None,
         }
     }
 }
