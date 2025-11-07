8b2370c8e78233abc452b4b71e9a2348afa7bd85
Jackson Parodi <jacksonaparodi@gmail.com>
2025-11-06 20:51:33 -0500

    change api endpoint

--- src/misc/constant.rs
+++ src/misc/constant.rs
@@ -1,14 +1,10 @@
-// pub const DONATIONS_LOG_BASE_ENDPOINT: &str = "log_donations.php";
-// pub const DONATIONS_PROCESS_NEW_ENDPOINT: &str = "process_new_donations.php";
 pub const DONATION_BASE_ENDPOINT: &str = "donation.php";
 pub const FLINTSTONE_BASE_ENDPOINT: &str = "flintstone.php";
 pub const MARKOV_ENDPOINT: &str = "markov.php";
 pub const RANDOM_OBLIQUE_STRAT_ENDPOINT: &str = "random_oblique_strat.php";
 
 pub const X_API_HEADER: &str = "X-Api-Key";
-// pub const NEW_DONATION_URI: &str = "new_donation";
-// pub const OLD_DONATION_URI: &str = "old_donation";
-pub const JPCOM_API_BASE_URL: &str = "https://www.jacksonparodi.com/php/api/";
+pub const JPCOM_API_BASE_URL: &str = "https://www.jacksonparodi.com/api/";
 
 pub const FLINTSTONES_DEFAULT_COUNT: u64 = 455;
 pub const OBLIQUE_STRAT_DEFAULT: &str = "Use an default colour";
