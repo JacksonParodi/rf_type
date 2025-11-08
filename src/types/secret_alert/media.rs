c954b6182c3750fb055022cf3adb6bdc50740b6d
Jackson Parodi <jacksonaparodi@gmail.com>
2025-11-08 17:39:53 -0500

    media types

--- src/types/secret_alert/media.rs
+++ src/types/secret_alert/media.rs
@@ -1,10 +1,21 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, PartialEq, Hash, Eq)]
+pub struct NonvideoMedia {
+    pub audio_filename: String,
+    pub image_filename: String,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Hash, Eq)]
+pub struct VideoMedia {
+    pub video_filename: String,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Hash, Eq)]
 /// the relevant filenames with no extensions
 pub enum SecretAlertMedia {
     // video filename
-    Video(String),
+    Video(VideoMedia),
     // (audio filename, image filename)
-    Nonvideo((String, String)),
+    Nonvideo(NonvideoMedia),
 }
