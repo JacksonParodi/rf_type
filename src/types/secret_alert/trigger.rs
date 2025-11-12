77973736f36345c5f9868e7ebbe843ac0ef339f6
Jackson Parodi <jacksonaparodi@gmail.com>
2025-11-12 00:05:24 -0500

    some conversions from twitch_api

--- src/types/secret_alert/trigger.rs
+++ src/types/secret_alert/trigger.rs
@@ -1,4 +1,9 @@
 use serde::{Deserialize, Serialize};
+use tracing::warn;
+use twitch_api::{
+    eventsub::channel::chat::notification::{CommunitySubGift, Subscription},
+    types::SubscriptionTier,
+};
 
 #[derive(Serialize, Deserialize, Hash, Eq, PartialEq, Clone, Debug)]
 pub enum TwitchSubscriptionTier {
@@ -15,4 +20,28 @@ pub enum SecretAlertTrigger {
     GiftSubscription(u32),
     Subscription(TwitchSubscriptionTier),
     Raid,
+}
+
+impl From<Subscription> for TwitchSubscriptionTier {
+    fn from(sub: Subscription) -> Self {
+        match &sub.sub_tier {
+            SubscriptionTier::Tier1 => TwitchSubscriptionTier::Tier1,
+            SubscriptionTier::Tier2 => TwitchSubscriptionTier::Tier2,
+            SubscriptionTier::Tier3 => TwitchSubscriptionTier::Tier3,
+            _ => match sub.is_prime {
+                true => TwitchSubscriptionTier::Prime,
+                false => {
+                    warn!("unknown subscription tier, defaulting to Tier1");
+                    TwitchSubscriptionTier::Tier1
+                }
+            },
+        }
+    }
+}
+
+impl From<CommunitySubGift> for SecretAlertTrigger {
+    fn from(sub: CommunitySubGift) -> Self {
+        let total = sub.total.max(1) as u32;
+        SecretAlertTrigger::GiftSubscription(total)
+    }
 }
